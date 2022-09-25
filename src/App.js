da53f08a9e2ceb5df31e79116cc5eea173bd95ed
IrynaTer-UkrTo <irinater9@gmail.com>
2022-09-25 11:54:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState(0);
+  const {id, name, author, description, image, showMore} = data[books];
+  const [showText, setShowText] = useState(false);
+
+
+  const previousBook = () =>{
+    setBooks((books =>{
+      books --;
+      if(books < 0){
+        return data.length -1;
+      }
+      return books;
+    }))
+  }
+
+  const nextBook = () =>{
+    setBooks((books =>{
+      books++;
+      if(books > data.length -1){
+        books = 0;
+      }
+      return books;
+    }))
+  }
+
+  const showTextClick = (item) =>{
+    // books.map(item =>{
+    //   const {id, name, author, description, image, showMore} = item;
+    //   })
+    item.showMore = !item.showMore;
+    setShowText(!showText)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div >
+      {/* {books.map(item =>{
+      const {id, name, author, description, image, showMore} = item;
+      })} */}
+
+      <div className='container'>
+        <h1 className='h1-text'>Make Your Reading List</h1>
+      </div>
+      <div className='container'>
+        <button className='btn' onClick={previousBook}>⬰</button>
+      <div className='book-container'>
+        <div>
+        <img src={image} alt="book" width='250px' />
+        </div>
+        <div className='book-text-container'>
+        <h2 className='h2-text'>{name}</h2>
+        <h3 className='h3-text'>Author: {author}</h3>
+        <p className='p-text'>{showMore ? description : description.substring(0,100) + "..."}
+        <button onClick={ () => showTextClick(item)}>{showMore ? 'Show less' : 'Show more'}</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className='btn-add'>Add to your Favourite ♥</button>
+        </div>
+      </div>
+      <button className='btn' onClick={nextBook}>⇴</button>
+      </div>
+   <div className='container'>
+    <h2 className='h2-text'>My Favourite:</h2>
+   </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,57 @@
+import imageOne from './pictures/1.jpg';
+import imageTwo from './pictures/2.jpg';
+import imageThree from './pictures/3.jpg';
+import imageFour from './pictures/4.jpg';
+import imageFive from './pictures/5.jpg';
+import imageSix from './pictures/6.jpg';
+
+export const data =[
+    {
+        id:1,
+        name: '"The Meaning of Mariah Carey"',
+        author: 'Maria Carey with Michaela Angela Davis',
+        description: 'The book navigates the complex racial, social, cultural and familial tensions associated with Carey`s upbringing as a biracial woman in Long Island, New York. This is framed alongside first-hand descriptions of the singer`s personal and professional triumphs and struggles, and is interspersed with fragments of Carey`s songwriting output.The memoir was met with critical acclaim and commercial success, becoming a number-one New York Times Best Seller after its first week of release. To promote the memoir, Carey made a series of public appearances on a range of talk shows, including an interview with Oprah Winfrey on The Oprah Conversation.',
+        image: imageOne,
+        showMore:false
+    },
+    {
+        id:2,
+        name: '"Becoming"',
+        author: 'Michelle Obama',
+        description: 'Described by the author as a deeply personal experience,[3] the book talks about her roots and how she found her voice, as well as her time in the White House, her public health campaign, and her role as a mother.[4] The book is published by Crown and was released in 24 languages.[4] One million copies were donated to First Book, an American nonprofit organization which provides books to children. It was the highest-selling book published in the United States in 2018, setting the record 15 days after its publication, with over two million copies sold',
+        image: imageTwo,
+        showMore:false
+    },
+    {
+        id:3,
+        name: '"A woman makes a plan"',
+        author: 'Maye Mask',
+        description: 'As everyone who follows her obsessively on social media knows, Maye is a fount of frank and practical advice on how the choices you make in every decade can pay off in surprising, exciting ways throughout your life. In A Woman Makes a Plan, Maye shares experiences from her life conveying hard-earned wisdom on career (the harder you work, the luckier you get), family (let the people you love go their own way), health (there is no magic pill), and adventure (make room for discovery, but always be ready for anything). You can`t control all that happens in life, but you can have the life you want at any age. All you have to do is make a plan.',
+        image: imageThree,
+        showMore:false
+    },
+    {
+        id:4,
+        name: '"Margaret Thatcher: The autobiography"',
+        author: 'Margaret Thatcher',
+        description: 'Margaret Thatcher is the towering political figure of late-twentieth-century Great Britain. No other prime minister in modern times sought to change the British nation and its place in the world as radically as she did. Published in a single volume for the first time, Margaret Thatcher is the story of her remarkable life told in her own words--the definitive account of an extraordinary woman and consummate politician, bringing together her bestselling memoirs The Downing Street Years and The Path to Power. Writing candidly about her upbringing and early years and the formation of her character and values, she details the experiences that propelled her to the very top in a man`s world. She offers a riveting firsthand history of the major events, the crises and triumphs, during her eleven years as prime minister, including the Falklands War, the Brighton hotel bombing, the Westland affair, the final years of the Cold War, and her unprecedented three election victories. Thatcher`s judgments of the men and women she encountered during her time in power-from statesmen, premiers, and presidents to Cabinet colleagues-are astonishingly frank, and she recalls her dramatic final days in office with a gripping, hour-by-hour description from inside 10 Downing Street. Powerful, candid, and compelling, Margaret Thatcher stands as a testament to a great leader`s significant legacy.',
+        image: imageFour,
+        showMore:false
+    },
+    {
+        id:5,
+        name: '"Autobiography of the Queen"',
+        author: 'Emma Tennant',
+        description: 'Queen Elizabeth has gone AWOL from Balmoral, and no one can find her. Where has she gone and why? This winsome, if unlikely, account of the Queen`s disappearance recounts the monarch`s solo journey to the Caribbean island of St. Lucia to take possession of her recently purchased home in the idyllic-sounding Joli Estate. Here, after more than half a century on the throne, she will recall the years of her reign in peace and tranquility, in the guise of her new identity as Gloria Smith. But the house is no more than a muddy hole in the ground, and the Queen must punt. She is not used to asking for anything, carrying cash, fending for herself, introducing herself, or being ignored. The story of the sovereign`s sojourn in her former colony is a funny and touching account of the sometimes contentious and occasionally baffling friendship between Her Majesty Queen Elizabeth and a scruffy young St. Lucian, Austin Ford. How this radical change of life and circumstance affects the Queen-and how she is transformed as a result-make for a hilarious and moving tale, in which her need for her subjects proves to be as strong as their desire that she remain on her throne',
+        image: imageFive,
+        showMore:false
+    },
+    {
+        id:6,
+        name: '"The Inspirational Life Story of Oprah Winfrey"',
+        author: 'Gregory Watson',
+        description: 'Oprah is best known for her award winning talk show "The Oprah Winfrey Show". She is also ranked as the richest African-American of the 20th century. With her massive success, most people would not think that she actually came from a humble beginning. This book will take you through her journey from being born into poverty to finally rising above all and becoming the billionaire today. Within Oprah`s past struggles, there are truly inspirational lessons to be learned. Up till today, Oprah continues to help millions around the world by giving them inspiration to better themselves.',
+        image: imageSix,
+        showMore:false
+    }
+]
